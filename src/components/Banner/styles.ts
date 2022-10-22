a90b24ccdee203f9fbbb2a7b4ce88ab085dcce8a
Gthalles <garbelottithalles@gmail.com>
2022-10-22 01:44:29 -0300

    Adiciona parte do componente de banner.

---
+++ src/components/Banner/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import colors from "../../styles/colors";
+
+export const BannerContainer = styled.div`
+    display: flex;
+    margin: auto;
+    max-width: 1440px;
+    background-color: ${colors.secondary};
+`;
+
+export const Banner = styled.div`
+    display: flex;
+    align-items: center;
+    min-width: 300px;
+    width: 50%;
+    background-color: #e4e9ea;
+`;
