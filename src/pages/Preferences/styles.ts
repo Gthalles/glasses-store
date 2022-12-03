31fb6d5645705825122974bf5ecf5f83dfa330d5
Gthalles <garbelottithalles@gmail.com>
2022-12-03 02:02:51 -0300

    Add initial version of preferences page.

--- src/pages/Preferences/styles.ts
+++ src/pages/Preferences/styles.ts
@@ -1,5 +1,20 @@
-import styled from "styled-components";
+import styled from "@emotion/styled";
 
-export const ZZZ = styled.div`
+interface IFormControl {
+    width?: string;
+}
 
+export const FormControl = styled.div<IFormControl>`
+    display: flex;
+    flex-direction: column;
+    width: ${({ width }) => width ? width : "calc(50% - 16px)"};
+    justify-content: space-between;
+    margin: 8px;
+`;
+
+export const CheckboxControl = styled.div<IFormControl>`
+    display: flex;
+    align-items: center;
+    margin: 8px;
+    width: ${({ width }) => width ? width : "calc(50% - 16px)"};
 `;
